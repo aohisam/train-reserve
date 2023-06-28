96a1111daa944b73614087a4c4c1f9d6eb184150
ohisama <second.2053.tjpamw@outlook.jp>
2023-06-28 16:23:31 +0900

    create_loginPage_link2

--- resources/views/layouts/login-link.blade.php
+++ resources/views/layouts/login-link.blade.php
@@ -5,12 +5,13 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>トップページ</title>
+    <title>ログイン</title>
 </head>
 
 <body>
-    <h2>トップページ</h2>
-    @include('layouts.login-link')
+    <div class="login-link">
+        <a href="{{ route('login.index') }}">ログインページ</a>
+    </div>
 </body>
 
 </html>
